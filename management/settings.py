dd9775f6f89f278fb630ea60ff36d1d32a8fa617
Teepakraaj <gteepak2003@gmail.com>
2025-07-02 13:46:10 +0530

    ver-3.1

--- management/settings.py
+++ management/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-)rs@nb!+x1!miiir0!z3tzdu(c7f0i^u$nj0#9#0!i1jc@n&$k
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ['retail-shop-accounting-software-1.onrender.com']
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['retail-shop-accounting-software-1.onrender.com']
 
 
 CSRF_TRUSTED_ORIGINS = ['https://retail-shop-accounting-software-1.onrender.com']
